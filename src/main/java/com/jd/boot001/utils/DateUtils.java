6e337149959d424ab832700def682bfd2a510239
zwhly986 <zwh_ly@163.com>
2024-07-28 18:26:45 +0800

    初始化

--- src/main/java/com/jd/boot001/utils/DateUtils.java
+++ src/main/java/com/jd/boot001/utils/DateUtils.java
@@ -1,4 +1,17 @@
 package com.jd.boot001.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 日期工具类
+ */
 public class DateUtils {
+    public static final String STD_DATETIME = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String date() {
+        return new SimpleDateFormat(STD_DATETIME).format(new Date());
+    }
+
 }
+
