a853c2d989041dec28ede44f279254af284fea29
zwhly986 <zwh_ly@163.com>
2024-08-01 11:51:06 +0800

    mapper优化

--- src/main/java/com/jd/boot001/mapper/Bank1AccountInfoMapper.java
+++ src/main/java/com/jd/boot001/mapper/Bank1AccountInfoMapper.java
@@ -8,12 +8,15 @@ import org.springframework.stereotype.Repository;
  * 用户账户操作
  * RocketMQ测试用例
  */
-//@Repository
-//@Mapper
+// 入口类已经加注解@MapperScan，不需要下面2个注解
+@Repository // spring注解，生成bean，被spring管理，被引用的地方不会标红
+// @Mapper // mybatis注解，被引用的地方会标红，
+// 三者区别见：https://blog.csdn.net/weixin_45188218/article/details/138085639
 public interface Bank1AccountInfoMapper {
 
     /**
      * 账户余额修改
+     *
      * @param accountNo
      * @param amount
      * @return
@@ -23,11 +26,22 @@ public interface Bank1AccountInfoMapper {
 
     /**
      * 查询事务id是否存在（用于RocketMQ事务回查）
+     *
      * @param txNo
      * @return
      */
     @Select("select count(1) from de_duplication_bank1 where tx_no = #{txNo}")
     int isExistTx(String txNo);
+
+    /**
+     * 新增事务id（用于RocketMQ事务回查）
+     *
+     * @param accountChangeEvent
+     * @return
+     */
+    @Insert("insert into de_duplication_bank1(tx_no, create_time, from_account, to_account, amount) " +
+            "values (#{ev.txNo},now(),#{ev.fromAccountNo}, #{ev.toAccountNo}, #{ev.amount});")
+    int addTx(@Param("ev") AccountChangeEvent accountChangeEvent);
 
     /**
      * 新增事务id（用于RocketMQ事务回查）
@@ -37,11 +51,4 @@ public interface Bank1AccountInfoMapper {
 //    @Insert("insert into de_duplication_bank1(tx_no, create_time, from_account, to_account, amount) values (#{txNo},now(),#{fromAccount}, #{toAccount}, #{amount});")
 //    int addTx(@Param("txNo") String txNo, @Param("fromAccount") String fromAccount, @Param("toAccount") String toAccount, @Param("amount") Double amount);
 
-    /**
-     * 新增事务id（用于RocketMQ事务回查）
-     * @param accountChangeEvent
-     * @return
-     */
-    @Insert("insert into de_duplication_bank1(tx_no, create_time, from_account, to_account, amount) values (#{ev.txNo},now(),#{ev.fromAccountNo}, #{ev.toAccountNo}, #{ev.amount});")
-    int addTx(@Param("ev") AccountChangeEvent accountChangeEvent);
 }
--- src/main/java/com/jd/boot001/mapper/Bank2AccountInfoMapper.java
+++ src/main/java/com/jd/boot001/mapper/Bank2AccountInfoMapper.java
@@ -14,6 +14,7 @@ public interface Bank2AccountInfoMapper {
 
     /**
      * 账户余额修改
+     *
      * @param accountNo
      * @param amount
      * @return
@@ -23,11 +24,22 @@ public interface Bank2AccountInfoMapper {
 
     /**
      * 查询事务id是否存在（用于RocketMQ事务回查）
+     *
      * @param txNo
      * @return
      */
     @Select("select count(1) from de_duplication_bank2 where tx_no = #{txNo}")
     int isExistTx(String txNo);
+
+    /**
+     * 新增事务id（用于RocketMQ事务回查）
+     *
+     * @param accountChangeEvent
+     * @return
+     */
+    @Insert("insert into de_duplication_bank2(tx_no, create_time, from_account, to_account, amount) " +
+            "values (#{ev.txNo},now(),#{ev.fromAccountNo}, #{ev.toAccountNo}, #{ev.amount});")
+    int addTx(@Param("ev") AccountChangeEvent accountChangeEvent);
 
     /**
      * 新增事务id（用于RocketMQ事务回查）
@@ -37,12 +49,4 @@ public interface Bank2AccountInfoMapper {
 //    @Insert("insert into de_duplication_bank2(tx_no, create_time, from_account, to_account, amount) values(#{txNo},now(),#{fromAccount}, #{toAccount}, #{amount});")
 //    int addTx(@Param("txNo") String txNo, @Param("fromAccount") String fromAccount, @Param("toAccount") String toAccount, @Param("amount") Double amount);
 
-
-    /**
-     * 新增事务id（用于RocketMQ事务回查）
-     * @param accountChangeEvent
-     * @return
-     */
-    @Insert("insert into de_duplication_bank2(tx_no, create_time, from_account, to_account, amount) values (#{ev.txNo},now(),#{ev.fromAccountNo}, #{ev.toAccountNo}, #{ev.amount});")
-    int addTx(@Param("ev") AccountChangeEvent accountChangeEvent);
 }
